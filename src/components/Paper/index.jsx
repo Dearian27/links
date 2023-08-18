56cd7ac84bde71feda21fdf909eeb437a19a4764
Dearian <dearian2706@gmail.com>
2023-08-18 15:43:32 +0300

    add link to instagram

--- src/components/Paper/index.jsx
+++ src/components/Paper/index.jsx
@@ -18,7 +18,7 @@ export default function Paper() {
         Записуйся на <span style={{background: "#e4dcd2", borderRadius: 5, padding: "1px 4px", width: "max-content"}}>БЕЗКОШТОВНЕ</span> пробне заняття!
       </h5>
       <Button link="https://forms.gle/WZDyXtrAM4CJ3SUL6">Запис на заняття</Button>
-      <Button link="">Instagram</Button>
+      <Button link="https://instagram.com/solution_online_school?igshid=MzRlODBiNWFlZA==">Instagram</Button>
       <Button link="http://www.tiktok.com/@solution.online.school">TikTok</Button>
       <Button link="https://www.facebook.com/dana.eng.lish.l">Facebook</Button>
       <Button link="https://t.me/english_with_dana_23">Telegram</Button>
