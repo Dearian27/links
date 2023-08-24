dc4797dcfc68c4369770fb56fa2718122d582e4f
Dearian <dearian2706@gmail.com>
2023-08-24 16:34:54 +0300

    change links, review button

--- src/components/Paper/index.jsx
+++ src/components/Paper/index.jsx
@@ -18,10 +18,11 @@ export default function Paper() {
         Записуйся на <span style={{background: "#e4dcd2", borderRadius: 5, padding: "1px 4px", width: "max-content"}}>БЕЗКОШТОВНЕ</span> пробне заняття!
       </h5>
       <Button link="https://forms.gle/WZDyXtrAM4CJ3SUL6">Запис на заняття</Button>
-      <Button link="https://instagram.com/solution_online_school?igshid=MzRlODBiNWFlZA==">Instagram</Button>
+      <Button link="https://instagram.com/solution_online_eng_school?igshid=OGQ5ZDc2ODk2ZA==">Instagram</Button>
       <Button link="http://www.tiktok.com/@solution.online.school">TikTok</Button>
       <Button link="https://www.facebook.com/dana.eng.lish.l">Facebook</Button>
-      <Button link="https://t.me/english_with_dana_23">Telegram</Button>
+      <Button link="https://t.me/eng_lish_with_dana">Telegram</Button>
+      <Button link="https://m.facebook.com/story.php?story_fbid=pfbid02Cy1xmC85NzR2crhST51KRSMY85YGt2w28vyAF2Y27uSawS8YuPvUQhK4FiTTofjYl&id=100070174384155">Відгуки</Button>
     </div>
   )
 }
